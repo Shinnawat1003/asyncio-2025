3c304114eb7fca52a0b34d96464286be40bf9640
Shinnawat1003 <156092155+Shinnawat1003@users.noreply.github.com>
2025-06-25 10:31:46 +0700

    assignment03

--- assignment03/asyncio01.py
+++ assignment03/asyncio01.py
@@ -1,1 +1,10 @@
-# check the type of a coroutine
+# check the type of a corutine
+import asyncio
+#define a coroutine
+async def custom_coro():
+    #await another coroutine
+    await asyncio.sleep(1)
+#create the corutine
+coro = custom_coro()
+#check the type of the corutine
+print(type(coro))
--- assignment03/asyncio02.py
+++ assignment03/asyncio02.py
@@ -1,2 +1,12 @@
 # example of running a coroutine
-
+import asyncio
+#define a coroutine
+async def custom_coro():
+    #await another
+    await asyncio.sleep(1)
+#main coroutine
+async def main():
+    #execute my custom coroutine
+    await custom_coro()
+#stat the coroutine program
+asyncio.run(main())
